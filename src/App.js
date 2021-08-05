b9d76941189c9ae1222c26d02d1e1dd02ca6c8a3
rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2021-08-05 16:46:50 +0600

    adding dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+import Signin from "./containers/Signin";
+import Signup from "./containers/Signup";
+import PrivateRoute from "./components/HOC/PrivateRoute";
+import { connect } from "react-redux";
+import React, { Component } from "react";
+import { isUserLoggedIn } from "./store/actions";
+import Product from "./containers/Products/index";
+import Order from "./containers/Orders/index";
+import Category from "./containers/Category/index";
+import Page from './containers/Page/index'
+import { getAllCategory } from "./store/actions";
+import { getInitialData } from "./store/actions/initialData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapStateToProps = (state) => {
+  return {
+    ...state,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    isUserLoggedIn: () => dispatch(isUserLoggedIn()),
+    getAllCategory: () => dispatch(getAllCategory()),
+    getInitialData: () => dispatch(getInitialData()),
+  };
+};
+
+class App extends Component {
+  state = {
+    flag: true
+  }
+  componentDidMount() {
+    this.props.isUserLoggedIn();
+  }
+  render() {
+    if (this.props.authReducers.authenticate) {
+      <Redirect to={"/mern-flipkart-backend/"} />;
+    }
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
+          <PrivateRoute exact path="/products" component={Product} />
+          <PrivateRoute exact path="/orders" component={Order} />
+          <PrivateRoute exact path="/categories" component={Category} />
+          <PrivateRoute path="/page" component={Page} />
+          <PrivateRoute exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
